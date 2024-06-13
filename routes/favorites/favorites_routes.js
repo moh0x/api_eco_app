d00b4622723b5666a16a13172a15296f8ace07e5
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-13 05:21:37 -0700

    Update favorites_routes.js

--- routes/favorites/favorites_routes.js
+++ routes/favorites/favorites_routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const favoriteController = require('../../controllers/favorites/favorites_controller');
 const { verifyToken } = require("../../utility/verify_token");
 router.patch('/changeFavorites',verifyToken,favoriteController.changeFavorites);
+router.get('/myFavorites',verifyToken,favoriteController.getUserFavorites);
   module.exports = 
     router
