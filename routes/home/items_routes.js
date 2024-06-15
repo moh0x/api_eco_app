58ec8d40685b984b3d98b864c0547cc3ad6bdf8b
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-15 02:02:09 -0700

    Update items_routes.js

--- routes/home/items_routes.js
+++ routes/home/items_routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { verifyToken } = require("../../utility/verify_token");
 const itemsControoler = require('../../controllers/home/items_controller');
 router.get('/getAllItems',verifyToken,itemsControoler.getAllItems);
+router.get('/getSearchItem',verifyToken,itemsControoler.getSearchItems);
 module.exports = 
 router
