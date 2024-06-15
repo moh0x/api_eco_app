df11226eb934f5a8d61802115d8c14be2a51a922
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-15 02:00:21 -0700

    Update items_controller.js

--- controllers/home/items_controller.js
+++ controllers/home/items_controller.js
@@ -9,6 +9,21 @@ const getAllItems = async(req,res)=>{
      res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
     }
  }
+ const getSearchItems = async(req,res)=>{
+   try {
+    var token =  req.headers.token;
+    var textItem = req.body.textItem;
+    const regex =new RegExp(textItem,'i');
+    const items = await Item.find({
+     $or:[
+      {itemNameArabic:regex},{itemNameEnglish:regex}
+     ]
+    });
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
  module.exports = {
-    getAllItems
+    getAllItems,getSearchItems
    }
