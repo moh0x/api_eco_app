a7a5e48e8db574829049512e22dde3c253551a0d
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-20 13:10:17 -0700

    Update items_controller.js

--- controllers/home/items_controller.js
+++ controllers/home/items_controller.js
@@ -9,6 +9,42 @@ const getAllItems = async(req,res)=>{
      res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
     }
  }
+ const getLikesItems = async(req,res)=>{
+   try {
+    var token =  req.headers.token;
+    const limit = 1;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const items = await Item.find({itemActive:true}).sort({itemLikesCount:-1}).limit(limit).skip(skip);
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
+const getCartItems = async(req,res)=>{
+   try {
+    var token =  req.headers.token;
+    const limit = 1;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const items = await Item.find({itemActive:true}).sort({itemCartCount:-1}).limit(limit).skip(skip);
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
+const getLatestItems = async(req,res)=>{
+   try {
+    var token =  req.headers.token;
+    const limit = 1;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const items = await Item.find({itemActive:true}).sort({itemDate:-1}).limit(limit).skip(skip);
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
  const getSearchItems = async(req,res)=>{
    try {
     var token =  req.headers.token;
@@ -25,5 +61,5 @@ const getAllItems = async(req,res)=>{
    }
 }
  module.exports = {
-    getAllItems,getSearchItems
+    getAllItems,getSearchItems,getLikesItems,getCartItems,getLatestItems
    }
