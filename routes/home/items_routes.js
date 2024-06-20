ccb3e58898b2ffbfca7a8e1a60ddc2bd467cc750
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-20 13:09:30 -0700

    Update items_routes.js

--- routes/home/items_routes.js
+++ routes/home/items_routes.js
@@ -4,5 +4,8 @@ const { verifyToken } = require("../../utility/verify_token");
 const itemsControoler = require('../../controllers/home/items_controller');
 router.get('/getAllItems',verifyToken,itemsControoler.getAllItems);
 router.get('/getSearchItem',verifyToken,itemsControoler.getSearchItems);
+router.get('/getLikesItem',verifyToken,itemsControoler.getLikesItems);
+router.get('/getCartItem',verifyToken,itemsControoler.getCartItems);
+router.get('/getLatestItem',verifyToken,itemsControoler.getLatestItems);
 module.exports = 
 router
