0155f0ae0b22ac9248804aad2465eee6f4be8cbc
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-24 13:15:11 -0700

    Update favorites_controller.js

--- controllers/favorites/favorites_controller.js
+++ controllers/favorites/favorites_controller.js
@@ -6,43 +6,43 @@ const changeFavorites = async(req,res)=>{
     const token = req.headers.token;
     const itemId = req.body.itemId;
     const user = await User.findOne({token:token},{__v:false,password:false});
+    const userId = user.id;
     const item = await Item.findById(itemId);
-    if (user.favorites.includes(itemId)) {
-        const indexItem = user.favorites.indexOf(itemId);
-        const newFav = user.favorites.splice(indexItem,1);
-        await User.findOneAndUpdate({token:token},{
+    if (item.itemLikesArray.includes(userId.toString())) {
+        const newFav = [];
+       for (let index = 0; index < item.itemLikesArray.length; index++) {
+       if (item.itemLikesArray[index] != userId) {
+        newFav.unshift(item.itemLikesArray[i] );
+       }
+        
+       }
+       const newItemArr =  await Item.findByIdAndUpdate(itemId,{
             $set:{
-                favorites:newFav
+                itemLikesArray:newFav
             }
         });
         const number= item.itemLikesCount - 1;
-        const newItem=  await Item.findOneAndUpdate({_id:itemId},{
+        const newItem=  await Item.findByIdAndUpdate(itemId,{
             $set:{
                 itemLikesCount:number
             }
         })
         await newItem.save();
-        await user.save();
-        const newUser = await User.findOne({token:token},{__v:false,password:false});
-        res.status(200).json({"status":httpsStatus.SUCCESS,"data":newUser});
+        await newItemArr.save();
+        res.status(200).json({"status":httpsStatus.SUCCESS,"data":"delete favorites success"});
     } else {
-        const newFav = user.favorites.unshift(itemId);
-        await User.findOneAndUpdate({token:token},{
-            $set:{
-                favorites:newFav
-            }
-        });
+        const newItemArr =    item.itemLikesArray.push(userId);
       
         const number= item.itemLikesCount + 1;
-        const newItem=  await Item.findOneAndUpdate({_id:itemId},{
+        const newItem=  await Item.findByIdAndUpdate(itemId,{
             $set:{
                 itemLikesCount:number
             }
         })
         await newItem.save();
-        await user.save();
-        const newUser = await User.findOne({token:token},{__v:false,password:false});
-        res.status(200).json({"status":httpsStatus.SUCCESS,"data":newUser});
+        await item.save();
+       
+        res.status(200).json({"status":httpsStatus.SUCCESS,"data":"add favorite success"});
     }
     
    } catch (error) {
@@ -52,13 +52,14 @@ const changeFavorites = async(req,res)=>{
 }
 const getUserFavorites = async(req,res)=>{
    try {
+    const limit = 15;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
     const token = req.headers.token;
     const user = await User.findOne({token:token},{__v:false,password:false});
-    const favListItemsIds = user.favorites;
-  
-   const items = await Item.find({_id:{
-    $in:favListItemsIds
-   }});
+   const items = await Item.find({itemActive:true,itemLikesArray:{
+    $in:user._id.toString()
+   }}).limit(limit).skip(skip);
    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
    } catch (error) {
     console.log(error);
