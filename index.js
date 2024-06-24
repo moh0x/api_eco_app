f4eb6404afae1646a1b9b19c9e58382438469daf
moh0x <141264101+moh0x@users.noreply.github.com>
2024-06-24 13:26:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,16 @@ const bannerRoute = require('./routes/home/banners_routes');
 const itemRoute = require('./routes/home/items_routes');
 const favoriteRoute = require('./routes/favorites/favorites_routes');
 const cartRoute = require('./routes/cart/cart_routes');
+const adressRoute = require('./routes/adress/adress_routes');
+const orderRoute = require('./routes/order/order_routes');
 app.use('/api/users/',userRoute);
 app.use('/api/home/categories',categoryRoute);
 app.use('/api/home/items',itemRoute);
 app.use('/api/home/banners',bannerRoute);
 app.use('/api/favorites',favoriteRoute);
 app.use('/api/carts',cartRoute);
+app.use('/api/adress',adressRoute);
+app.use('/api/order',orderRoute);
 app.listen(port);
  mongoose.connect("mongodb+srv://mihoyahou:YzFN4ccF8u7HkKHi@cluster0.jjdh5dp.mongodb.net/");
 /*
@@ -26,4 +30,3 @@ app.listen(port);
       2) Make sure you allow ES module imports: Add "type": "module" to package.json file
       
       ; */
-
